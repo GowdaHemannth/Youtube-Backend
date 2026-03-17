7a45e6f19e93b7b2d57c37e7ee31e0e2881c6526
GowdaHemannth <hemanthagalahalli@gmail.com>
2026-03-17 12:45:13 +0530

    DATABASE FILE AND CONNECTION ADDED

---
+++ src/index.js
@@ -0,0 +1,36 @@
+//  dot env loads all the env varibles Once the File Starts
+// Here in Package.json We HAVE MENTIONE TAHT INDEX.JS IS THE STARTING FILE 
+//  Its Good Practice to use the dot env in the index.js
+import mongoose from "mongoose";
+import { DB_NAME } from "./constants";
+import express from "express";
+// There is One More Thing in Javascript called emp
+// Here What Happens IN the Sense there are some function where you need to call that fnction as soon as the 
+//  fucntion created
+
+// HERE INSTEAD OF WRITTING THE WHOLE CODE YOU CAN ACTUALLY Write these Code in the Seperate File Thing
+// Like You Can Actaully Write Thes ein Seperate File Whereyou can Add these Things  
+
+// const app=express()
+
+// (async()=>{
+//      try{
+//          await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+//         //  Here below code signifies that Express is not able to Listen to Db Thats the Issue Here
+//          app.on("error",(error)=>{
+//             console.log("EXPRESS NOT ABLE TO LISTEN TO DB:",error);
+            
+
+//          })
+//         //   Here if the Express App IS LISTENING 
+//         app.listen(process.env.PORT,()=>{
+//             console.log("DB CONNECTED APP IS LISTENING WELL AND GOOD ");
+            
+//         })
+//      }
+//      catch(error){
+//         console.log("Error Appeared As we were connecting To DATABSE:",error);
+        
+
+//      }
+// })()
