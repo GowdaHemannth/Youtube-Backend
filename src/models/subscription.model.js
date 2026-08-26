976d49a70aa2bd5e90648a508216e926751410a4
GowdaHemannth <hemanthagalahalli@gmail.com>
2026-08-26 22:44:14 +0530

    Add subscription model with subscriber reference

--- src/models/subscription.model.js
+++ src/models/subscription.model.js
@@ -1,5 +1,10 @@
 import mongoose,{Schema, schema} from  "mongoose"
 
 const subscriptionSchema= new Schema({
-    
+    subscriber:{
+        type:Schema.Types.ObjectId,
+       ref:"User"
+    }
 })  
+
+export const Subscription=mongoose.model("Subscription",subscriptionSchema)
